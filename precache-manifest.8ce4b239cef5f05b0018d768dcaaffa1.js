5ce6b5162cbcb2eabf299e5b734ce0bac408b368
DavidDMPP <davidvlogindo@gmail.com>
2020-08-13 21:09:43 +0700

    Updates

---
+++ precache-manifest.8ce4b239cef5f05b0018d768dcaaffa1.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c67f09c475885e2ceb81bb9d1a3f189",
+    "url": "/ReactApp/index.html"
+  },
+  {
+    "revision": "7eb20758d958cd5d6275",
+    "url": "/ReactApp/static/css/main.0b0fd139.chunk.css"
+  },
+  {
+    "revision": "2b185a892b981da60ecc",
+    "url": "/ReactApp/static/js/2.1ac4567f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactApp/static/js/2.1ac4567f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7eb20758d958cd5d6275",
+    "url": "/ReactApp/static/js/main.733d4a17.chunk.js"
+  },
+  {
+    "revision": "029ea820835dacbd0148",
+    "url": "/ReactApp/static/js/runtime-main.7ce5ac92.js"
+  },
+  {
+    "revision": "b997820129b3ab5d80f5abd43161bd61",
+    "url": "/ReactApp/static/media/clients-01.b9978201.svg"
+  },
+  {
+    "revision": "e54005bf054d03d33bd1b34a89fed3e7",
+    "url": "/ReactApp/static/media/clients-02.e54005bf.svg"
+  },
+  {
+    "revision": "bbaa54a04b69de04ee674773e09302b1",
+    "url": "/ReactApp/static/media/clients-03.bbaa54a0.svg"
+  },
+  {
+    "revision": "cdf0e500e4741076b6ee2e9787b4b445",
+    "url": "/ReactApp/static/media/clients-04.cdf0e500.svg"
+  },
+  {
+    "revision": "b913b15068e1b11eb206a101eda47921",
+    "url": "/ReactApp/static/media/clients-05.b913b150.svg"
+  },
+  {
+    "revision": "6dbc36133e254d171e27c7165309c05c",
+    "url": "/ReactApp/static/media/feature-tile-icon-01.6dbc3613.svg"
+  },
+  {
+    "revision": "a02bfd4b6b5aa8cd6dc156002599a75a",
+    "url": "/ReactApp/static/media/feature-tile-icon-02.a02bfd4b.svg"
+  },
+  {
+    "revision": "91b1b862e40b185edf9082c364243790",
+    "url": "/ReactApp/static/media/feature-tile-icon-03.91b1b862.svg"
+  },
+  {
+    "revision": "687180834e71818255e449ec6ed473c0",
+    "url": "/ReactApp/static/media/hero-image.68718083.png"
+  },
+  {
+    "revision": "4c66333f28a69b611caa3ab78e437a0b",
+    "url": "/ReactApp/static/media/illustration-element-01.4c66333f.svg"
+  },
+  {
+    "revision": "ad9b81a8361d1ca5d6b49b80dd00035e",
+    "url": "/ReactApp/static/media/illustration-section-01.ad9b81a8.svg"
+  },
+  {
+    "revision": "f062941c87f1c6075df1bcb7f49d85e4",
+    "url": "/ReactApp/static/media/logo.f062941c.svg"
+  }
+]);
